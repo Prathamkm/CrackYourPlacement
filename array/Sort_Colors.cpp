c663ca5d0a4da791708b684d6370f8a380365e63
Pratham kumar <prathamkumar6699@gmail.com>
2024-07-28 18:45:44 +0530

    array 7

---
+++ array/Sort_Colors.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int start = 0, end = nums.size() - 1, mid = 0;
+        while (mid <= end)
+        {
+            if (nums[mid] == 2)
+            {
+                swap(nums[mid], nums[end]);
+                end--;
+            }
+            else if (nums[mid] == 0)
+            {
+                swap(nums[mid], nums[start]);
+                start++;
+                mid++;
+            }
+            else
+                mid++;
+        }
+    }
+};
